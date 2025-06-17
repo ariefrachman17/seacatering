042bc4f5b1b9bc4b5db01713e60e982d990a379b
ariefrachman17 <ariefrachman2023@student.unas.ac.id>
2025-06-17 21:39:19 +0700

    feat(home): add Livewire home page view

---
+++ resources/views/livewire/home-page.blade.php
@@ -0,0 +1,89 @@
+<div>
+    @vite('resources/css/app.css')
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <div class="w-full bg-gradient-to-r from-softgreen to-cream py-10 px-4 sm:px-6 lg:px-8 mx-auto">
+        <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8">
+            <!-- Grid -->
+            <div class="grid md:grid-cols-2 gap-4 md:gap-8 xl:gap-20 md:items-center">
+                <div>
+                    <h1 class="block text-3xl font-bold text-gray-800 sm:text-4xl lg:text-6xl lg:leading-tight ">SEA
+                        Catering
+                    </h1>
+                    <h2 class="block text-xl font-semibold sm:text-2xl lg:text-3xl lg:leading-tight text-primary">Healthy
+                        Meals, Anytime, Anywhere</h2>
+                    <p class="mt-3 text-lg text-gray-800">Welcome to SEA Catering your trusted partner for customizable,
+                        nutritious meals delivered straight to your door, anywhere in Indonesia.</p>
+
+                    <!-- icons -->
+                    <div class="grid grid-cols-2 gap-6 text-sm text-gray-800 pt-5">
+                        <div class="flex items-center gap-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960"
+                                width="24px" fill="#10b981">
+                                <path
+                                    d="M280-80v-366q-51-14-85.5-56T160-600v-280h80v280h40v-280h80v280h40v-280h80v280q0 56-34.5 98T360-446v366h-80Zm400 0v-320H560v-280q0-83 58.5-141.5T760-880v800h-80Z" />
+                            </svg>
+                            <span>Menu Plan</span>
+                        </div>
+
+                        <div class="flex items-center gap-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960"
+                                width="24px" fill="#10b981">
+                                <path
+                                    d="m136-240-56-56 296-298 160 160 208-206H640v-80h240v240h-80v-104L536-320 376-480 136-240Z" />
+                            </svg>
+                            <span>Nutrition Info</span>
+                        </div>
+
+                        <div class="flex items-center gap-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960"
+                                width="24px" fill="#10b981">
+                                <path
+                                    d="M280-160q-50 0-85-35t-35-85H60l18-80h113q17-19 40-29.5t49-10.5q26 0 49 10.5t40 29.5h167l84-360H182l4-17q6-28 27.5-45.5T264-800h456l-37 160h117l120 160-40 200h-80q0 50-35 85t-85 35q-50 0-85-35t-35-85H400q0 50-35 85t-85 35Zm357-280h193l4-21-74-99h-95l-28 120Zm-19-273 2-7-84 360 2-7 34-146 46-200ZM20-427l20-80h220l-20 80H20Zm80-146 20-80h260l-20 80H100Zm180 333q17 0 28.5-11.5T320-280q0-17-11.5-28.5T280-320q-17 0-28.5 11.5T240-280q0 17 11.5 28.5T280-240Zm400 0q17 0 28.5-11.5T720-280q0-17-11.5-28.5T680-320q-17 0-28.5 11.5T640-280q0 17 11.5 28.5T680-240Z" />
+                            </svg>
+                            <span>Delivery throughout Indonesia</span>
+                        </div>
+
+                        <div class="flex items-center gap-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960"
+                                width="24px" fill="#10b981">
+                                <path
+                                    d="M440-501Zm0 381L313-234q-72-65-123.5-116t-85-96q-33.5-45-49-87T40-621q0-94 63-156.5T260-840q52 0 99 22t81 62q34-40 81-62t99-22q81 0 136 45.5T831-680h-85q-18-40-53-60t-73-20q-51 0-88 27.5T463-660h-46q-31-45-70.5-72.5T260-760q-57 0-98.5 39.5T120-621q0 33 14 67t50 78.5q36 44.5 98 104T440-228q26-23 61-53t56-50l9 9 19.5 19.5L605-283l9 9q-22 20-56 49.5T498-172l-58 52Zm280-160v-120H600v-80h120v-120h80v120h120v80H800v120h-80Z" />
+                            </svg>
+                            <span>Healthy Food</span>
+                        </div>
+                    </div>
+                    <!-- end icons -->
+
+
+
+                    <!-- Buttons -->
+                    <div class="mt-7 flex flex-col sm:flex-row gap-3 w-full">
+                        <a class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-primary text-white hover:bg-primary-dark disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                            href="#">
+                            Order Now
+                            <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24"
+                                height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                stroke-linecap="round" stroke-linejoin="round">
+                                <path d="m9 18 6-6-6-6" />
+                            </svg>
+                        </a>
+                        <a class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                            href="/contact">
+                            Contact sales team
+                        </a>
+                    </div>
+                    <!-- End Buttons -->
+                </div>
+                
+                <!-- End Col -->
+                <div class="relative flex justify-center sm:justify-start ms-0">
+                    <img class="w-full sm:max-w-md md:max-w-lg rounded-md" src="{{ asset('img/hero.jpg') }}"
+                        alt="Image Description">
+                </div>
+
+            </div>
+            <!-- End Grid -->
+        </div>
+    </div>
+</div>
