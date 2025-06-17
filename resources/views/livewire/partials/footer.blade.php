ac1f6255c67bc4c9a49d963eb6d5230409c4d27d
ariefrachman17 <ariefrachman2023@student.unas.ac.id>
2025-06-17 21:38:55 +0700

    feat(partial): add footer component

---
+++ resources/views/livewire/partials/footer.blade.php
@@ -0,0 +1,74 @@
+<footer class="bg-secondary w-full">
+    <div class="w-full max-w-[85rem] py-8 px-4 sm:px-6 lg:px-6 lg:pt-15 mx-auto">
+        <!-- Grid -->
+        <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4">
+            <div class="col-span-2 md:col-span-1">
+                <div class="flex items-center gap-2">
+                    <img src="{{ asset('img/logo4.svg') }}" alt="logo4" class="h-15 w-auto">
+                    <a class="flex-none text-xl font-semibold text-white dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                        href="#" aria-label="Brand">SEA Catering</a>
+                </div>
+                <p class="inline-flex gap-x-2 font-semibold text-gray-400 text-sm">Healthy Meals, Anytime, Anywhere</p>
+            </div>
+
+            <!-- End Col -->
+
+            <div class="col-span-1">
+                <h4 class="font-semibold text-gray-100">Quick Links</h4>
+
+                <div class="mt-3 grid space-y-3">
+                    <p><a class="inline-flex gap-x-2 font-semibold text-gray-400 hover:text-gray-300 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                            href="/home">Home</a></p>
+                    <p><a class="inline-flex gap-x-2 font-semibold text-gray-400 hover:text-gray-300 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                            href="/menu">Menu Plans</a></p>
+                    <p><a class="inline-flex gap-x-2 font-semibold text-gray-400 hover:text-gray-300 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                            href="/subscription">Subscription</a></p>
+                    <p><a class="inline-flex gap-x-2 font-semibold text-gray-400 hover:text-gray-300 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                            href="/contact">Contact</a></p>
+                </div>
+            </div>
+            <!-- End Col -->
+
+            <div class="col-span-1">
+                <h4 class="font-semibold text-gray-100">Services</h4>
+
+                <div class="mt-3 grid space-y-3">
+                    <p><a class="inline-flex gap-x-2 font-semibold text-gray-400 hover:text-gray-300 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                            href="#">Diet Plan</a></p>
+                    <p><a class="inline-flex gap-x-2 font-semibold text-gray-400 hover:text-gray-300 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                            href="#">Protein Plain</a></p>
+                    <p><a class="inline-flex gap-x-2 font-semibold text-gray-400 hover:text-gray-300 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                            href="#">Royal Plan</a></p>
+                    <p><a class="inline-flex gap-x-2 font-semibold text-gray-400 hover:text-gray-300 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                            href="#">Custom Plan</a></p>
+                </div>
+            </div>
+            <!-- End Col -->
+
+            <div class="col-span-1">
+                <h4 class="font-semibold text-gray-100">Follow Us</h4>
+                <!-- Social Brands -->
+                <div>
+                    <a class="w-10 h-10 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-white hover:bg-white/10 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:ring-1 focus:ring-gray-600"
+                        href="#">
+                        <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="16"
+                            height="16" fill="currentColor" viewBox="0 0 16 16">
+                            <path
+                                d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
+                        </svg>
+                    </a>
+                </div>
+                <!-- End Social Brands -->
+            </div>
+            <!-- End Col -->
+        </div>
+        <!-- End Grid -->
+
+        <div class="mt-5 sm:mt-12 grid gap-y-2 sm:gap-y-0 sm:items-center">
+            <div class="flex justify-between items-center">
+                <p class="text-sm font-semibold text-gray-400">© 2025 SEACatering. All rights reserved.</p>
+            </div>
+            <!-- End Col -->
+        </div>
+    </div>
+</footer>
