a7457b38f0ec7983e6b4e58add3328e44edb580d
ariefrachman17 <ariefrachman2023@student.unas.ac.id>
2025-06-29 21:43:00 +0700

    update(layout): update app layout blade view

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,20 +4,25 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <title>{{ $title ?? 'SEA Catering' }}</title>
   @vite(['resources/css/app.css', 'resources/js/app.js'])
   @livewireStyles
+
 </head>
 
 <body class="bg-slate-200 dark:bg-slate-700">
   @livewire('partials.navbar')
-  {{-- <main class="w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto"> --}}
+  
   <main>
     {{ $slot }}
   </main>
+  
   @livewire('partials.footer')
+  
   @livewireScripts
+
 </body>
 
 </html>
